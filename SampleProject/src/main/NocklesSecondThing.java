ac04b5756d31112c9ba33a918a17fd5eb1404814
Nockles <Nockles@10.8.33.99>
2017-02-02 12:21:56 -0500

    Made a 2nd file

---
+++ SampleProject/src/main/NocklesSecondThing.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Nockles
+ *
+ */
+public class NocklesSecondThing {
+
+	/**
+	 * 
+	 */
+	public NocklesSecondThing() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
