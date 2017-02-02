0e32bab65a6eee1e1df1bc0e0d0a5e40d4fdb7bd
Nockles <Nockles@10.8.33.99>
2017-02-02 12:19:45 -0500

    Created a Nockles File

---
+++ SampleProject/src/main/NocklesThing.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Nockles
+ *
+ */
+public class NocklesThing {
+
+	/**
+	 * 
+	 */
+	public NocklesThing() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
